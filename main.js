a42f1ef193b175bf4b49409f0629e62c339abe07
LindaCai5 <>
2019-09-14 16:51:20 -0400

    Cookies added

--- main.js
+++ main.js
@@ -3,3 +3,28 @@ $(document).ready(function() {
 		e.stopPropagation();
 	});
 });
+
+// Get the modal
+var modal1 = document.getElementById("exampleModalCenter");
+
+
+
+window.onclick = function(event) {
+  if (event.target == modal1) {
+    modal1.style.display = "none";
+  }
+}
+
+
+
+// ---------------------------------------------------------
+
+
+
+
+$(document).ready(function() {
+
+  $('#cookieModal').modal('show');
+	setTimeout(function() {$('#cookieModal').modal('hide');},
+	 4000);
+});
